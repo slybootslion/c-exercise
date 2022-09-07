b127d71630ecd7e936df761b9440a93e85a19a3e
slybootslion <slybootslion@vip.qq.com>
2022-09-07 11:36:56 +0800

    2022-09-07 11:36:56

--- imooc/basic/practice/w06-01-二叉树/1-14.c
+++ imooc/basic/practice/w06-01-二叉树/1-14.c
@@ -5,104 +5,76 @@
 #include<stdlib.h>
 #include<string.h>
 
-#define START printf("=====start=====\n")
-#define END printf("=====end=====\n")
-#define toByte(n) ((n) / 8 + ((n) % 8 > 0))
+typedef struct {
+    char *code;
+    unsigned long size;
+} huff_buf, *huff_bufp;
 
-typedef struct HuffListNode HuffListNode, *hufflistnodep;
-typedef struct HuffNode HuffNode, *huffnodep;
-typedef struct HuffTree HuffTree, *hufftreep;
-typedef struct HuffCode HuffCode, *huffcodep;
-typedef struct HuffList HuffList, *hufflistp;
-typedef struct HuffResult HuffResult, *huffresultp;
-typedef struct HuffCode HuffBuf, *huffbufp;  //缓存类型
-
-struct HuffListNode {
-    huffnodep node;     //huffman节点
-    hufflistnodep next; //后继节点
-};  //huffman频率节点
-
-struct HuffList {
-    hufflistnodep head;         //头结点
-    int keys[256];              //键值字典
-    int size;                   //链表长度
-};
+typedef struct HuffListNode huff_list_node, *huff_list_nodep;
+typedef struct HuffNode huff_node, *huff_nodep;
+typedef struct HuffCode huff_code, *huff_codep;
 
 struct HuffNode {
-    int key;            //键
-    int weight;         //权重
-    huffnodep left;     //左节点
-    huffnodep right;    //右节点
-};  //huffman节点
+    int key;
+    int weight;
+    huff_nodep left;
+    huff_nodep right;
+};
+
+struct HuffListNode {
+    huff_nodep node;
+    huff_list_nodep next;
+};
 
 struct HuffCode {
-    char *code; //huffman code
-    int size;   //huffman code size
+    char *code;
+    int size;
 };
 
-struct HuffTree {
-    huffnodep root;         //根
-    huffcodep codes[256];   //key对应的代码
-    int size;               //大小
-};  //huffman树
+typedef struct {
+    huff_nodep root;
+    huff_codep codes[256];
+    int size;
+} huff_tree, *huff_treep;
 
-struct HuffResult {
-    char *code;      //生成的代码
-    hufftreep tree;  //对应的霍夫曼树
-};
+typedef struct {
+    char *code;
+    huff_treep tree;
+} huff_result, *huff_resultp;
 
-typedef enum {
-    FALSE = 0,
-    TRUE = 1,
-} Boolean;
+typedef struct {
+    huff_list_nodep head;
+    int keys[256];
+    int size;
+} huff_list, *huff_listp;
 
-huffnodep huffnode(int key, int weight); //初始化huffman节点
-hufflistp hufflist();   //初始化hufflist
-Boolean insertHuffNode(hufflistp list, huffnodep node);  //向指定的节点链表添加一个节点
-huffnodep shiftHuffNode(hufflistp list);    //删除第一个节点
-hufftreep hufftree(hufflistp list);   //构建一棵huffman tree
-huffbufp getFileBuf(const char *filename); //获取文件的buf
-hufftreep genhuffcodes(hufftreep tree, huffnodep node, char codes[], int idx);  //获取当前节点之下的节点的huffman编码
-hufflistp getHuffListByFile(const char *filename); //根据文件创建huffman链表
-hufflistp getHuffListByBuf(huffbufp buf); //根据文件buf创建huffman链表
-huffcodep getHuffCode(hufftreep tree, int key);  //获取指定键值的huffcode
-huffresultp getHuffCodesByFile(const char *filename);     //获取文件的huffman code
-huffresultp getHuffCodesByBuf(huffbufp buf);    //通过buf获取codes
-huffbufp getOriginBuf(huffresultp result);       //从result中解析出原始的字符串
-huffbufp str2bin(char *str); //二进制字符串转二进制数组
-int putOriginToFile(huffresultp result, const char *filename);   //将result存储到filename中
-char *bin2str(huffbufp buf);    //二进制数组转二进制字符串
-huffbufp readHuffFile(const char *filename); //解析huff文件
-
-huffnodep huffnode(int key, int weight) {
-    huffnodep ret = (huffnodep) malloc(sizeof(HuffNode));
-    ret->key = key;
-    ret->weight = weight;
-    ret->left = ret->right = NULL;
-
-    return ret;
+huff_nodep make_huff_node(int key, int weight) {
+    huff_nodep res = (huff_nodep) malloc(sizeof(huff_node));
+    res->key = key;
+    res->weight = weight;
+    res->left = res->right = NULL;
+    return res;
 }
 
-hufflistp hufflist() {
-    hufflistp ret = (hufflistp) malloc(sizeof(HuffList));
-    ret->head = NULL;
-    memset(ret->keys, 0, sizeof(ret->keys[0]) * 256);
-    ret->size = 0;
-
-    return ret;
+huff_listp make_huff_list() {
+    huff_listp res = (huff_listp) malloc(sizeof(huff_list));
+    res->head = NULL;
+    memset(res->keys, 0, sizeof(res->keys[0]) * 256);
+    res->size = 0;
+    return res;
 }
 
-Boolean insertHuffNode(hufflistp list, huffnodep node) {
-    if (list == NULL || node == NULL || node->weight <= -256) return FALSE;
-    hufflistnodep cur = list->head;
-    hufflistnodep *rootp = &(list->head);
-    hufflistnodep *last = NULL; //当前指针的前驱指针
-    hufflistnodep tmp = (hufflistnodep) malloc(sizeof(HuffListNode));
-    tmp->node = node;
-    tmp->next = NULL;
-    if (node->key >= 0 && node->key < 256) {
-        list->keys[node->key] = node->weight;   //添加key到keys字典
-    }
+int insert_huff_node(huff_listp list, huff_nodep node) {
+    if (list == NULL || node == NULL || node->weight <= -256)
+        return 0;
+    huff_list_nodep cur = list->head;
+    huff_list_nodep *rootp = &(list->head);
+    huff_list_nodep *last = NULL;
+    huff_list_nodep temp = (huff_list_nodep) malloc(sizeof(huff_list_node));
+    temp->node = node;
+    temp->next = NULL;
+    if (node->key >= 0 && node->key < 256)
+        list->keys[node->key] = node->weight;
     list->size++;
 
     for (; cur != NULL && cur->node->weight < node->weight; cur = cur->next) {
@@ -110,387 +82,164 @@ Boolean insertHuffNode(hufflistp list, huffnodep node) {
         rootp = &(cur->next);
     }
 
-    tmp->next = cur;
-    if (last == NULL) {   //第一个元素
-        list->head = tmp;
-    } else {  //向当前节点前面插入tmp节点
-        (*last)->next = tmp;
-    }
+    temp->next = cur;
+    if (last == NULL)
+        list->head = temp;
+    else
+        (*last)->next = temp;
 
-    return TRUE;
+    return 1;
 }
 
-huffnodep shiftHuffNode(hufflistp list) {
-    if (list == NULL || list->head == NULL) return NULL;
-    huffnodep ret = list->head->node;
-    hufflistnodep next = list->head->next;
+huff_listp get_huff_list(huff_bufp buf) {
+    if (buf == NULL || buf->code == NULL)
+        return NULL;
+    huff_listp list = make_huff_list();
+    for (int i = 0; buf->code[i] != '\0'; ++i) {
+        unsigned char ch = buf->code[i];
+        list->keys[ch]++;
+    }
+    for (int i = 0; i < 256; ++i) {
+        if (list->keys[i] > 0)
+            insert_huff_node(list, make_huff_node(i, list->keys[i]));
+    }
+    return list;
+}
+
+huff_nodep shift_huff_node(huff_listp list) {
+    if (list == NULL || list->head == NULL)
+        return NULL;
+    huff_nodep res = list->head->node;
+    huff_list_nodep next = list->head->next;
     free(list->head);
     list->head = next;
     list->size--;
-
-    return ret;
+    return res;
 }
 
-//通过huffman list构建
-hufftreep hufftree(hufflistp list) {
-    hufftreep tree = (hufftreep) malloc(sizeof(HuffTree));
-    tree->root = NULL;
-    tree->size = 0;
-    memset(tree->codes, 0, sizeof(tree->codes));
-
-    huffnodep a = NULL;
-    huffnodep b = NULL;
-    huffnodep c = NULL;
-    tree->size = 2 * list->size - 1;
-    while (list->size > 1) {  //hufflistp长度大于1
-        a = shiftHuffNode(list);
-        b = shiftHuffNode(list);
-        c = huffnode(-256, a->weight + b->weight);    //新的节点
-        c->left = a;
-        c->right = b;
-        insertHuffNode(list, c); //将c压回list
-    }
-    tree->root = c;
-
-    //生成所有key的huffman编码
-    char codes[8092];   //huffman编辑路径
-
-    return genhuffcodes(tree, tree->root, codes, 0);
-}
-
-//获取文件内容的BUF
-huffbufp getFileBuf(const char *filename) {
-    FILE *fp = fopen(filename, "r");
-    if (fp == NULL) return NULL;
-    fseek(fp, 0L, SEEK_END);
-    int len = ftell(fp);
-    rewind(fp); //重设
-    huffbufp ret = (huffbufp) malloc(sizeof(HuffBuf));
-    ret->code = (char *) malloc(len + 1);
-    ret->size = len;
-    fread(ret->code, 1, len, fp);
-    fclose(fp);
-
-    return ret;
-}
-
-hufftreep genhuffcodes(hufftreep tree, huffnodep node, char codes[], int idx) {
-    if (tree == NULL || node == NULL) {   //到达底部
+huff_treep gen_huff_codes(huff_treep tree, huff_nodep node, char codes[], int idx) {
+    if (tree == NULL || node == NULL)
         return NULL;
-    }
-
-    if (node->left == NULL && node->right == NULL) {  //叶子节点
+    if (node->left == NULL && node->right == NULL) {
         int key = node->key;
-        huffcodep code = (huffcodep) malloc(sizeof(HuffCode));
+        huff_codep code = (huff_codep) malloc(sizeof(huff_code));
         code->code = (char *) malloc(idx + 1);
         code->size = idx;
         memcpy(code->code, codes, code->size);
         code->code[code->size] = '\0';
         tree->codes[key] = code;
     }
-    {
-        codes[idx] = '1'; //右
-        genhuffcodes(tree, node->right, codes, idx + 1);
-        codes[idx] = '0'; //左
-        genhuffcodes(tree, node->left, codes, idx + 1);
-    }
-
+    codes[idx] = '1';
+    gen_huff_codes(tree, node->right, codes, idx + 1);
+    codes[idx] = '0';
+    gen_huff_codes(tree, node->left, codes, idx + 1);
     return tree;
 }
 
-//通过文件生成huffman list
-hufflistp getHuffListByFile(const char *filename) {
-    huffbufp buf = getFileBuf(filename);
-    if (buf == NULL) return NULL;
+huff_treep make_huff_tree(huff_listp list) {
+    huff_treep tree = (huff_treep) malloc(sizeof(huff_tree));
+    tree->root = NULL;
+    tree->size = 0;
+    memset(tree->codes, 0, sizeof(tree->codes));
 
-    hufflistp list = getHuffListByBuf(buf);
-    free(buf->code);
-    buf->code = NULL;
-    free(buf);
-    buf = NULL;
-
-    return list;
+    huff_nodep a, b, c;
+    tree->size = 2 * list->size - 1;
+    while (list->size > 1) {
+        a = shift_huff_node(list);
+        b = shift_huff_node(list);
+        c = make_huff_node(-256, a->weight + b->weight);
+        c->left = a;
+        c->right = b;
+        insert_huff_node(list, c);
+    }
+    tree->root = c;
+    char codes[8092];
+    return gen_huff_codes(tree, tree->root, codes, 0);
 }
 
-hufflistp getHuffListByBuf(huffbufp buf) {
-    if (buf == NULL || buf->code == NULL) return NULL;
-
-    char *code = buf->code;
-
-    hufflistp list = hufflist();
-    for (int i = 0; code[i] != '\0'; i++) {
-        unsigned char ch = code[i];
-        list->keys[ch]++;
-    }
-
-    for (int i = 0; i < 256; i++) {
-        if (list->keys[i] > 0) {   //插入存在的字符
-            insertHuffNode(list, huffnode(i, list->keys[i]));
-        }
-    }
-
-    return list;
-}
-
-huffcodep getHuffCode(hufftreep tree, int key) {
-    if (key < 256 && key >= 0 && tree->codes[key] > 0) {
+huff_codep get_huff_code(huff_treep tree, int key) {
+    if (key < 256 && key >= 0 && tree->codes[key] > 0)
         return tree->codes[key];
-    }
     return NULL;
 }
 
-huffresultp getHuffCodesByFile(const char *filename) {
-    huffbufp buf = getFileBuf(filename);   //文件缓存
-    return getHuffCodesByBuf(buf);
-}
-
-huffresultp getHuffCodesByBuf(huffbufp buf) {
-    huffresultp result = (huffresultp) malloc(sizeof(HuffResult));
+huff_resultp huffman_encoding(huff_bufp buf) {
+    huff_resultp result = (huff_result *) malloc(sizeof(huff_result));
     result->code = NULL;
 
     if (buf == NULL) return NULL;
 
-    hufflistp list = getHuffListByBuf(buf); //huffman list
+    huff_listp list = get_huff_list(buf);
 
-    result->tree = hufftree(list);
-    int buf_len = buf->size;
+    result->tree = make_huff_tree(list);
     int len = 0;
-    for (int i = 0; buf->code[i] != '\0'; i++) {
+    for (int i = 0; buf->code[i] != '\0'; ++i) {
         int key = (unsigned char) buf->code[i];
-        huffcodep code = getHuffCode(result->tree, key);
-        if (code == NULL) {
-            printf("LLL:%c{%d}\n", key, key);
+        huff_codep code = get_huff_code(result->tree, key);
+        if (code == NULL)
             return NULL;
-        }
         len += code->size;
     }
     result->code = (char *) malloc(len + 1);
     result->code[0] = '\0';
-    for (int i = 0; buf->code[i] != '\0'; i++) {
+
+    for (int i = 0; buf->code[i] != '\0'; ++i) {
         unsigned char key = buf->code[i];
-        huffcodep code = getHuffCode(result->tree, key);
+        huff_codep code = get_huff_code(result->tree, key);
         strncat(result->code, code->code, code->size);
     }
 
     return result;
 }
 
-huffbufp getOriginBuf(huffresultp result) {
-    if (result == NULL || result->code == NULL || result->tree == NULL) return NULL;
-    hufftreep tree = result->tree;
+huff_bufp huffman_decoding(huff_resultp result) {
+    if (result == NULL || result->code == NULL || result->tree == NULL)
+        return NULL;
+    huff_treep tree = result->tree;
     char *code = result->code;
     int len = 0;
     for (int i = 0; code[i] != '\0';) {
-        huffnodep root = tree->root;    //根节点
-        while (root->left != NULL && root->right != NULL && code[i] != '\0') {   //双子节点存在
+        huff_nodep root = tree->root;
+        while (root->left != NULL && root->right != NULL && code[i] != '\0') {
             root = (code[i] == '0' ? root->left : root->right);
             i++;
         }
-        if ((root->left != NULL || root->right != NULL) && code[i] == '\0') { //错误
+        if ((root->left != NULL || root->right != NULL) && code[i] == '\0')
             return NULL;
-        }
         len++;
-        // printf("解析:%c{%s}\n",root->key,tree->codes[root->key]->code);
     }
 
-    huffbufp ret = (huffbufp) malloc(sizeof(HuffBuf));
-    ret->code = (char *) malloc(len + 1);
-    ret->code[0] = '\0';
-    ret->size = len;
+    huff_bufp res = (huff_bufp) malloc(sizeof(huff_buf));
+    res->code = (char *) malloc(len + 1);
+    res->code[0] = '\0';
+    res->size = len;
 
     int idx = 0;
     for (int i = 0; code[i] != '\0';) {
-        huffnodep root = tree->root;    //根节点
-        while (root->left != NULL && root->right != NULL && code[i] != '\0') {   //双子节点存在
+        huff_nodep root = tree->root;
+        while (root->left != NULL && root->right != NULL && code[i] != '\0') {
             root = (code[i] == '0' ? root->left : root->right);
             i++;
         }
-        ret->code[idx++] = root->key;
+        res->code[idx++] = root->key;
     }
-    ret->code[idx] = '\0';
-
-    return ret;
-}
-
-int putOriginToFile(huffresultp result, const char *filename) {
-    if (result == NULL) return 0;
-    // printf("res1[%d]:%s\n",(int)strlen(result->code),result->code);
-    // huffbufp b = str2bin(result->code);
-    // printf("%d\n",b->size);
-    // printf("res2:%s\n",bin2str(b));
-    // return 0;
-
-    huffbufp buf = str2bin(result->code);   //huffman code转成buf
-    int i = 0;
-    int len = 0;
-    for (i = 0; i < 256; i++) {
-        if (result->tree->codes[i] > 0) { //
-            len += 5 + result->tree->codes[i]->size;   //key[1]:len[4]:size
-        }
-    }
-    huffbufp keys = (huffbufp) malloc(sizeof(HuffBuf));
-    keys->code = (char *) malloc(len);
-    keys->size = 0;
-    //获取keys
-    int idx = 0;
-    for (i = 0; i < 256; i++) {
-        if (result->tree->codes[i] > 0) { //
-            keys->code[idx++] = i;    //key
-            int len = result->tree->codes[i]->size;
-            memcpy(keys->code + idx, &len, 4);    //key size
-            // printf("%c[%d]:%d{%s}\n",i,i,len,result->tree->codes[i]->code);
-            idx += 4;
-            huffbufp tmp = str2bin(result->tree->codes[i]->code);
-            // printf("%d,%d\n",tmp->code[0],tmp->size);
-            int tsize = toByte(tmp->size);
-            memcpy(keys->code + idx, tmp->code, tsize);
-            idx += tsize;
-        }
-    }
-
-    keys->size = idx;   //诸多键的总空间
-
-    //写出标准文件
-    //HUF\n
-    //size: 4b
-    //keys
-    //size: 4b
-    //codes
-    FILE *fp = fopen(filename, "w");
-    if (fp == NULL) return -1;
-    fwrite("HUF\n", 1, 4, fp);
-    fwrite(&idx, 1, 4, fp);    //size
-    fwrite(keys->code, 1, keys->size, fp); //写入code
-    fwrite(&(buf->size), 1, 4, fp);    //size
-    fwrite(buf->code, 1, toByte(buf->size), fp);
-    fclose(fp);
-
-    return 4 + 4 + keys->size + 4 + buf->size;
-}
-
-
-huffbufp str2bin(char *str) { //二进制字符串转二进制数组
-    // printf("bin:%s\n",str);
-    if (str == NULL) return NULL;
-    huffbufp buf = (huffbufp) malloc(sizeof(HuffBuf));
-    int l = strlen(str);
-    int size = (l / 8) + (l % 8 > 0);
-
-    buf->code = (char *) malloc(l);
-    memset(buf->code, 0, l);
-    for (int i = 0; i < l; i++) {
-        int idx = i / 8;
-        int bi = i % 8;
-        buf->code[idx] |= (str[i] == '0' ? 0 : 1) << bi;
-    }
-    buf->size = l;
-
-    return buf;
-}
-
-char *bin2str(huffbufp buf) {
-    char *ret = (char *) malloc(buf->size + 1);
-    for (int i = 0; i < buf->size; i++) {
-        int idx = i / 8;
-        int offset = i % 8;
-        ret[i] = (buf->code[idx] & (0x01 << offset)) ? '1' : '0';
-    }
-    ret[buf->size] = '\0';
-
-    return ret;
-}
-
-huffbufp readHuffFile(const char *filename) {
-    huffbufp buf = getFileBuf(filename);
-    if (buf == NULL) return NULL;
-
-    if (memcmp(buf->code, "HUF\n", 4) != 0) return NULL;   //文件不以BUF\n开头
-    huffresultp result = (huffresultp) malloc(sizeof(HuffResult));
-    //BUF\n
-    //key size
-    int key_size = *(int *) (buf->code + 4);
-    int base = 8; //偏移量
-    hufftreep tree = (hufftreep) malloc(sizeof(HuffTree));
-    tree->root = NULL;
-    tree->size = 0;
-    huffcodep *codes = tree->codes;   //key对应代码
-    memset(codes, 0, sizeof(huffcodep) * 256);
-
-    int oft = 0;
-    for (; oft < key_size;) {
-        int offset = base + oft;
-        unsigned char key = buf->code[offset];
-        // printf("%d[%c]\n",key,key);
-        int size = *(int *) (buf->code + offset + 1); //长度
-        int byte = toByte(size);
-        huffbufp htmp = (huffbufp) malloc(sizeof(HuffBuf));
-        //键对应代码
-        htmp->code = buf->code + offset + 5;    //缓存代码
-        htmp->size = size;  //缓存大小
-        // printf("[%c]%d\n",key,key);
-        huffcodep tmp = (huffcodep) malloc(sizeof(HuffCode));
-        tmp->size = size;   //key的大小
-        tmp->code = bin2str(htmp);
-        tree->codes[key] = tmp;
-        tree->size++;   //树的大小增加
-        huffnodep root = tree->root;
-        if (root == NULL) {
-            tree->root = huffnode(-256, 0);
-            root = tree->root;
-        }
-        for (int i = 0; i < tmp->size; i++) {
-            char ch = tmp->code[i];
-            huffnodep node = NULL;
-            if (ch == '0') {
-                node = root->left;
-                if (node == NULL) {
-                    node = huffnode(-256, 0);
-                }
-                root->left = node;
-            } else {
-                node = root->right;
-                if (node == NULL) {
-                    node = huffnode(-256, 0);
-                }
-                root->right = node;
-            }
-            if (i == tmp->size - 1)
-                node->key = key;
-            root = node;
-        }
-        oft += 5 + byte;
-    }
-
-    huffbufp tmp = (huffbufp) malloc(sizeof(HuffBuf));
-    tmp->code = buf->code + base + oft + 4;
-    tmp->size = *(int *) (buf->code + base + oft);
-    // printf("tmp size:%d\n",tmp->size);
-    result->tree = tree;
-    result->code = bin2str(tmp);
-    // printf("%s\n",result->code);
-
-    // for(int i = 0; i < 256; i++){
-    //     if(codes[i]!=NULL){
-    //         printf("%c[%d]:%s\n",i,i,codes[i]->code);
-    //     }
-    // }
-
-    return getOriginBuf(result);
+    res->code[idx] = '\0';
+    return res;
 }
 
 int main() {
-//    char b[] = "ADDCDCCBCDDBDDD";
+    char b[] = "ADDCDCCBCDDBDDD";
+    printf("origin string is: %s\n", b);
 
-    huffbufp buf = (huffbufp) malloc(sizeof(HuffBuf));
+    huff_bufp buf = (huff_bufp) malloc(sizeof(huff_buf));
 
-    buf->code = "ADDCDCCBCDDBDDD";
+    buf->code = &b[0];
     buf->size = strlen(buf->code);
-    huffresultp result = getHuffCodesByBuf(buf);    //获取编码结果
-    printf("huffman: %s\n", result->code);
+    huff_resultp result = huffman_encoding(buf);    //获取编码结果
+    printf("huffman encode is: %s\n", result->code);
 
-    huffbufp origin = getOriginBuf(result); //通过编码获取原始数据
-    printf("origin: %s\n", origin->code);
-
+    huff_bufp origin = huffman_decoding(result); //通过编码获取原始数据
+    printf("huffman decode is: %s\n", origin->code);
     return 0;
 }
 
