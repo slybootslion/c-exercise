857d5ce4e8e864978c72f18af8a156fc1376fbf9
slybootslion <slybootslion@vip.qq.com>
2022-09-16 16:37:20 +0800

    2022-09-16 16:37:20

---
+++ c-lesson/Chapter4/05.recursion.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+unsigned int Factorial(unsigned int n) {
+  return n == 0 ? 1 : n * Factorial(n - 1);
+}
+
+unsigned int Fibonacci(unsigned int n) {
+  if (n == 1 || n == 0)
+    return n;
+  else
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+unsigned int FactorialByIteration(unsigned int n) {
+  unsigned int result = 1;
+  for (unsigned int i = n; i > 0; --i)
+    result *= i;
+  return result;
+}
+
+unsigned int FibonacciByIteration(unsigned int n) {
+  if (n == 1 || n == 0)
+    return n;
+  unsigned int current = 1;
+  unsigned int last = 0;
+  for (int i = 0; i < n - 1; ++i) {
+    unsigned temp = current;
+    current += last;
+    last = temp;
+  }
+  return current;
+}
+
+int main() {
+  printf("3! = %d\n", Factorial(3));
+  printf("5! = %d\n", Factorial(5));
+  printf("10! = %d\n", Factorial(10));
+
+  printf("3! = %d\n", FactorialByIteration(3));
+  printf("5! = %d\n", FactorialByIteration(5));
+  printf("10! = %d\n", FactorialByIteration(10));
+
+  printf("Fibonacci(3) = %d\n", Fibonacci(3));
+  printf("Fibonacci(5) = %d\n", Fibonacci(5));
+  printf("Fibonacci(10) = %d\n", Fibonacci(10));
+
+  printf("Fibonacci(3) = %d\n", FibonacciByIteration(3));
+  printf("Fibonacci(5) = %d\n", FibonacciByIteration(5));
+  printf("Fibonacci(10) = %d\n", FibonacciByIteration(10));
+  return 0;
+}
---
+++ c-lesson/Chapter4/06.hanoi.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void Move(int n, char src, char dest, char temp) {
+  if (n == 0)
+    return;
+  else if (n == 1)
+    printf("%c --> %c\n", src, dest);
+  else {
+    Move(n - 1, src, temp, dest);
+    Move(1, src, dest, temp);
+    Move(n - 1, temp, dest, src);
+  }
+}
+
+int main(void) {
+  Move(9, 'a', 'c', 'b');
+  return 0;
+}
