89bb106fdf789e75f041e58d16e48947be1e2d45
slybootslion <slybootslion@vip.qq.com>
2022-11-18 00:04:35 +0800

    c++的类

--- hm/03_c++/01_basic/02_c++调用c/c_file.c
+++ hm/03_c++/01_basic/02_c++调用c/c_file.c
@@ -1,5 +1,7 @@
 #include "c_file.h"
 
 void c_file_print() {
-  printf("--c file--\n");
+  printf("--c file C的文件--\n");
+//	我觉得还是不改了，毕竟要装一个我都不知道是什么的东西。但是这段东西，我放这里，万一以后要查看。
+// sudo apt-get install ibus-gtk ibus-gtk3 ibus-qt4
 }
