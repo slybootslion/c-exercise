dfa06693221a66c5fb6b7112659b1c28877a02eb
slybootslion_云桌面 <slybootslion@vip.qq.com>
2022-12-06 21:28:38 +0800

    map1

--- exercise/0_C++ STL 标准模板库(容器总结)算法/03_map_multimap/02_正反向遍历映射容器.cpp
+++ exercise/0_C++ STL 标准模板库(容器总结)算法/03_map_multimap/02_正反向遍历映射容器.cpp
@@ -4,6 +4,23 @@
 namespace MapA2 {
 // 通过使用begin/rbegin,end/rend等迭代器，实现对map映射容器元素的正反向遍历
 void test01() {
-  cout << "正反向遍历映射容器" << endl;
+  map<string, int> mp;
+  // 三种方式实现map容器插入操作
+  mp.insert(pair<string, int>("admin", 100));
+  mp.insert(make_pair("user", 200));
+  mp["guest"] = 300;
+
+  mp.erase("user"); // 删除第二个数据
+
+  // 正向遍历键值对
+  for (map<string, int>::iterator it = mp.begin(); it != mp.end(); it++) {
+	cout << "key = " << it->first << " --> value = " << it->second << endl;
+  }
+  print_line();
+
+  // 反向遍历键值对
+  for (map<string, int>::reverse_iterator rit = mp.rbegin(); rit != mp.rend(); rit++) {
+	cout << "key = " << rit->first << " --> value = " << rit->second << endl;
+  }
 }
 }
