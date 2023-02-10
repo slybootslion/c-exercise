ee19a586c9104dca96970240049d405f6a42a37e
slybootslion <slybootslion@vip.qq.com>
2023-02-10 10:16:07 +0800

    不知道

--- primer_exercise/niuke_cpp/composite.cpp
+++ primer_exercise/niuke_cpp/composite.cpp
@@ -99,10 +99,10 @@ namespace composite
 	// CPP23 计算公司年销售额
 	void cpp23() {
 		int arr[4][3] = {
-				{22, 66, 44},
-				{77, 33, 88},
-				{25, 45, 65},
-				{11, 66, 99}
+				{ 22, 66, 44 },
+				{ 77, 33, 88 },
+				{ 25, 45, 65 },
+				{ 11, 66, 99 }
 		};
 		int sum = 0;
 		for (int i = 0; i < 4; i++) {
@@ -113,12 +113,140 @@ namespace composite
 		cout << sum << endl;
 	}
 
+	// CPP24 字符串拼接
+	void cpp24() {
+		string s1, s2;
+		getline(cin, s1);
+		getline(cin, s2);
+		cout << s1 + s2 << endl;
+	}
+
+	// CPP56 字符的个数
+	void cpp56() {
+		string s;
+		cin >> s;
+		int a = 0, b = 0, c = 0;
+		for (auto ch: s) {
+			switch (ch) {
+			case 'a':
+				a++;
+				break;
+			case 'b':
+				b++;
+				break;
+			case 'c':
+				c++;
+				break;
+			default:
+				break;
+			}
+		}
+		cout << a << ' ' << b << ' ' << c;
+	}
+
+	// CPP25 结构体简单使用
+	struct student
+	{
+		string name;
+		unsigned age{};
+		double height{};
+	};
+
+	void cpp25() {
+		string name;
+		unsigned age;
+		double height;
+		cin >> name >> age >> height;
+		struct student s;
+		s.name = name;
+		s.age = age;
+		s.height = height;
+		cout << s.name << ' ' << s.age << ' ' << s.height;
+	}
+
+	// CPP26 利用指针遍历数组
+	void cpp26() {
+		int arr[6] = { 0 };
+		int* ptr = arr;
+		int len = sizeof(arr) / sizeof(int);
+		for (int i = 0; i < len; i++)
+			cin >> arr[i];
+		while (ptr <= &arr[5]) {
+			cout << *ptr << " ";
+			ptr++;
+		}
+	}
+
+	// CPP27 获取字符串长度
+	void cpp27() {
+		char str[100] = { 0 };
+		cin.getline(str, sizeof(str));
+
+		// write your code here......
+		int c = 0;
+		char* p = str;
+		while (*p != '\0') {
+			c++;
+			p++;
+		}
+		cout << c;
+	}
+
+	// CPP28 复制部分字符串
+	void cpp28() {
+		char str[100] = { 0 };
+		cin.getline(str, sizeof(str));
+		int c = 0;
+		char* p = str;
+		while (*p != '\0') {
+			c++;
+			p++;
+		}
+		cout << c;
+	}
+
+	// CPP29 创建动态数组
+	void cpp29() {
+		int n;
+		cin >> n;
+		int arr[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = n + i;
+		}
+		for (int i: arr)
+			cout << i << ' ';
+	}
+
+	// CPP57 创建二维动态数组
+	void cpp57() {
+		int n;
+		cin >> n;
+		int arr[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++)
+				arr[i][j] = i + j;
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++)
+				cout << arr[i][j] << ' ';
+			cout << endl;
+		}
+	}
+
 	void main() {
 //		cpp19();
 //		cpp20();
 //		cpp21();
 //		cpp22();
-		cpp23();
+//		cpp23();
+//		cpp24();
+//		cpp56();
+//		cpp25();
+//		cpp26();
+//		cpp27();
+//		cpp28();
+//		cpp29();
+		cpp57();
 
 	}
 }
